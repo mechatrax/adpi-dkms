1eaa43f16424b138d9358d180cc7e5a7327edebb
Masahiro Honda <honda@mechatrax.com>
2025-04-25 11:49:52 +0900

    Improve compatibility

--- pre_build.sh
+++ pre_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SRC=$(apt-cache depends linux-source | grep -o -E "linux-source-[0-9]+(\.[0-9]+)*" | head -n1)
+VER=$(echo $1 | grep -o -E "^([0-9]+\.[0-9]+)")
+SRC=linux-source-$VER
 
 tar -xf /usr/src/$SRC.tar.xz $SRC/drivers/iio/adc/{ad7793,ad_sigma_delta}.c --xform=s,$SRC/drivers/iio/adc,., 
 
-for p in patches/*.patch
+for p in patches/$VER/*.patch
 do
   patch -p1 < $p
 done
